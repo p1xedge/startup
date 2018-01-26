322f06a9c786e9540afd64f76e91cc65d4e0ebda
p1xedge <35808838+p1xedge@users.noreply.github.com>
2018-01-26 10:01:13 +0530

    status.sh
    This is a DASHBOARD

---
+++ status.sh
@@ -0,0 +1,12 @@
+
+#!/bin/bash
+clear
+echo _________________welcome to p1xedge_____________________
+
+vcgencmd get_mem arm && vcgencmd get_mem gpu
+vcgencmd measure_temp
+hostname -I
+
+
+echo *******************checkout****************************
+echo ______________-____yuzit.in____-________________________
